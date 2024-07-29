a09e68884fd42732e94c8c78038ea6ef5a14f47b
exuanbo <exuanbo@protonmail.com>
2024-07-29 12:57:54 +0800

    test: disable escaping string

--- __tests__/jest.config.base.js
+++ __tests__/jest.config.base.js
@@ -3,5 +3,8 @@ const path = require('path')
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/*.ts'],
-  rootDir: path.dirname(__dirname)
+  rootDir: path.dirname(__dirname),
+  snapshotFormat: {
+    escapeString: false
+  }
 }
